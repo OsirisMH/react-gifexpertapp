5d6e241bce3db366cfdd20f1e3f13fe726097b59
Osiris Meza <oamh09@gmail.com>
2021-08-23 04:31:19 -0600

    Eliminadas las variables de entorno propias, README actualizado

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async ( category ) => {
-    const url = `https://api.giphy.com/v1/gifs/search?limit=15&q=${ encodeURI( category ) }&api_key=LxX4yfK6d51e96RWUDHqnjWie31Q0EHZ`;
+    const url = `https://api.giphy.com/v1/gifs/search?limit=15&q=${ encodeURI( category ) }&api_key=${ process.env.REACT_APP_API_KEY }`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
